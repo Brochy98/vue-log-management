18fa4e5eea25a91814801a19fcff3798af4b0ee7
Broch <brochphone@gmail.com>
2025-06-02 10:03:26 +0200

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full bg-zinc-300">
+    <div class="min-h-screen flex flex-col">Text</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup() {},
+})
+</script>
+
+<style scoped></style>
---
+++ src/components/TableLogs.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Table">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style scoped></style>
